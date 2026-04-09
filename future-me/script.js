ac3a788b1a076b6e7336e4dc1931bf2cd11947d3
SravanSakha <sravansakha@gmail.com>
2026-04-09 20:55:17 +0530

    Update script.js

--- future-me/script.js
+++ future-me/script.js
@@ -7,9 +7,12 @@ function generate() {
   let name = document.getElementById("name").value;
   let goal = document.getElementById("goal").value;
 
-  let result = `${name}, in 2035 you became a successful ${goal}! 🚀`;
+  let year = Math.floor(Math.random() * (2099 - 2025 + 1)) + 2025;
+
+  let result = `${name}, in ${year} you became a successful ${goal}! 🚀`;
 
   document.getElementById("screen2").style.display = "none";
   document.getElementById("screen3").style.display = "flex";
   document.getElementById("result").innerText = result;
 }
+
